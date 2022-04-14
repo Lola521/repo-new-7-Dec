092f51b1d4cabc7f2f80998f3fccb3b50aabc55e
guole jing <guole.jing@gmail.com>
2022-04-14 22:38:10 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mandarin = Restaurant.create([{ name: 'Le mandarin' }, { category: 'chinese' }, { address: '13001 Marseille' }])
+goodprice = Review.create(content: 'great place, good price', rating: '5')
+goodprice.restaurant = mandarin
+goodprice.save
+altante = Restaurant.create([{ name: 'Al tente' }, {category: 'italian' }, { address: '13002 Marseille' }])
+wasabi = Restaurant.create([{ name: 'Wasabi' }, {category: 'japanese' }, { address: '13003 Marseille' }])
