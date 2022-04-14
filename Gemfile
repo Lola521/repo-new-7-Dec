076cf05ce63e83c345f47e3caee229973aab22a8
guole jing <guole.jing@gmail.com>
2022-04-14 12:03:59 +0200

    rails new

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.4'
+ruby '3.0.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'rails', '~> 6.1.5'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
@@ -43,9 +43,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'rspec-rails', group: [ :test ]
-gem 'rails-controller-testing', group: [ :test ]
-# Gemfile
-gem 'autoprefixer-rails'
-gem 'font-awesome-sass', '~> 5.6.1'
-gem 'simple_form'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
-Restaurant.destroy_all
-Review.destroy_all
-
-puts "Creating restaurants..."
-
-restaurants = Restaurant.create([
-  { name: 'Le Mandarin', address: '5 cours Jean Ballard 13001 Marseille', category: 'chinese' },
-  { name: 'Sushi Room', address: '20 rue vagon 13001 Marseille', category: 'japanese' },
-  { name: 'Rome', address: 'rome Italy', category: 'italian' },
-  { name: 'Chips', address: 'Bruge ', category: 'belgian' },
-  { name: 'Raphael', address: 'Plage pointe rouge 13009 marseille', category: 'french' }
-  ])
-
-Review.create(content: 'Great place to try', rating: 5, restaurant: restaurants.first)
-Review.create(content: 'Great place to try', rating: 5, restaurant: restaurants.last)
-Review.create(content: 'Great place to try', rating: 5, restaurant: restaurants[1])
-puts "Finished!"
